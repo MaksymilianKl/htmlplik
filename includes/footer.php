c1bd1a19a0e53464f11d59b5df44893c87602094
MaksymilianKl <131866916+MaksymilianKl@users.noreply.github.com>
2025-06-09 00:18:40 +0200

    Delete includes directory

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-  <img src="assets/img/logo.png" class="logo" alt="shopg logo" style="width:120px; margin-bottom:10px; display:block; margin-left:auto; margin-right:auto;">
-</div>
-</body>
-</html> 
